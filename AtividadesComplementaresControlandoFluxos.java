50a41f7155e16670536e5cc37956786773609058
isademelo <demelodev@gmail.com>
2026-05-09 14:54:57 -0300

    Refactor AtividadesComplementaresControlandoFluxos

--- AtividadesComplementaresControlandoFluxos.java
+++ AtividadesComplementaresControlandoFluxos.java
@@ -48,6 +48,8 @@ class DescobrirSeEhNegativoOuPositivo {
     }
 }
 
+
+
 //3- Crie um menu que oferece duas opções ao usuário: "1. Calcular área do quadrado" e "2. Calcular área do círculo". Solicite a escolha do usuário e realize o cálculo da área com base na opção selecionada.
 class CalculadoraGeometrica{
     public static void calcularGeometria(){
